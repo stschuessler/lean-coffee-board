7009a610171f5ccc799db0d8cf89cd04fc80739c
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-27 15:08:15 +0200

    Include UTF8

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
+  res.setHeader('Content-Type', 'text/html; charset=utf-8') // charset UTF 8 hier mit einfügen wegen der Umlaute
+  res.end('<h1>Hellö, World!</h1>')
 })
 
 server.listen(port, () => {
