b629cc32b364687baca4608b0a3530637339d442
Sarah Schüßler <stschuessler@googlemail.com>
2021-09-30 08:46:22 +0200

    Set versionKey false

--- models/Card.js
+++ models/Card.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose') // require mongoose
 
-const schema = new mongoose.Schema({
-  // festlegen, welche Daten sollen in unserer Datenbank vorhanden sein  // als Objekt angelegt
-  text: {
-    type: String, // hierin ein Objekt anlegen, welches definiert, wie dieses text-Objekt gesetzt sein muss
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    // festlegen, welche Daten sollen in unserer Datenbank vorhanden sein  // als Objekt angelegt
+    text: {
+      type: String, // hierin ein Objekt anlegen, welches definiert, wie dieses text-Objekt gesetzt sein muss
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
   },
-  author: {
-    type: String,
-    required: true,
-  },
-})
+  { versionKey: false } // verhindert, dass mongoose einen Version key mitliefert. Dieser wird autm. für alle Objekte angelegt, die wir in Mongo anlegen wollen
+)
 
 //Collection: 'cards'
 //Mongoose Model: 'Card' --> 'cards'
